ae5b15dcfb2de4ce43edce7358bc59fff1986ac4
py20044 <78597548+py20044@users.noreply.github.com>
2023-02-08 12:15:51 +0000

    ugh

--- ContextBandit.py
+++ ContextBandit.py
@@ -2,13 +2,15 @@ import numpy as np
 import random
 
 
+
 class ContextBandit:
-    def __init__(self, lambdas):  # lambdas is a list of functions that take context vector x and return a reward
+    def __init__(self, lambdas, noise):  # lambdas is a list of functions that take context vector x and return a reward
         self.k = len(lambdas)
         self.lambdas = lambdas
+        self.noise = noise
         self.regret = 0
 
     def pull(self, a, x):
         rewards = [reward(x) for reward in self.lambdas]
         self.regret += max(rewards) - rewards[a]
-        return rewards[a]
+        return rewards[a] + self.noise(x)
---
+++ SimKNN.py
@@ -0,0 +1,83 @@
+from pathos.multiprocessing import ProcessingPool as Pool
+import numpy
+import math as m
+from scipy.spatial.distance import cdist
+
+import ContextBandit
+
+
+def sim_knn(bandit, X, theta, phi):
+    n = len(X)
+    regrets = []
+    n_arms = bandit.k  # number of arms
+    actions = []
+    rewards = []
+    assert (n >= n_arms)
+    for a in range(n_arms):  ##play each arm once regardless of context
+        reward = bandit.pull(a, X[a])
+        rewards.append(reward)
+        actions.append(a)
+        regrets.append(bandit.regret)
+    for t in range(n_arms, n, 1):
+        distances = cdist(X[:t], [X[t]], 'Euclidean')
+        distances = [dist for sublist in distances for dist in sublist]
+        k_a = numpy.zeros(n_arms)
+        index = numpy.zeros(n_arms)  ## to store I_t,k
+        for a in range(n_arms):  ##evaluate k_a for all a
+            k_a[a] = k_ternary_search(actions, rewards, distances, theta, phi, a)
+            k_least = numpy.argpartition(distances, int(k_a[a] - 1))[:int(k_a[a])]
+            N = sum(numpy.array(actions)[k_least] == a)
+            S = sum(numpy.array(rewards)[k_least] * (numpy.array(actions)[k_least] == a))
+            f_hat = S / N if (N != 0) else 0
+            index[a] = f_hat + m.sqrt(theta * m.log(t) / N) + phi(t) * distances[k_least[-1]] if (N != 0) else m.inf
+        arm = numpy.argmax(index)
+        reward = bandit.pull(arm, X[t])
+        regrets.append(bandit.regret)
+        actions.append(arm)
+        rewards.append(reward)
+    return regrets
+
+
+def uncertainty(k, actions, rewards, distances, theta, phi, a):
+    t = len(actions)
+    k_least = numpy.argpartition(distances, k - 1)[:k]
+    N = sum(numpy.array(actions)[k_least] == a)
+    uncer = m.sqrt(theta * m.log(t) / N) + phi(t) * distances[k_least[-1]] if (N != 0) else m.inf
+    return uncer
+
+
+def k_ternary_search(actions, rewards, distances, theta, phi, a):
+    f = lambda k: uncertainty(k, actions, rewards, distances, theta, phi, a)
+    t = len(actions) - 1
+    l = 1
+    r = t
+    while True:
+        c1 = l + m.floor((r - l) / 3)
+        c2 = l + 2 * m.floor((r - l) / 3)
+        if f(c1) > f(c2):
+            l = c1
+        elif f(c1) < f(c2):
+            r = c2
+        else:
+            if c1 == c2:
+                return c1
+            else:
+                l, r = c1, c2
+        if abs(l - r) <= 1:
+            break
+    return m.floor((l + r) / 2)
+
+
+if (__name__ == "__main__"):
+    X = numpy.random.uniform(0, 1, size=(10000, 2))
+    lambdas = [lambda x: numpy.prod([m.sin(4 * xs * m.pi) for xs in x]),
+               lambda x: numpy.prod([m.cos(7 * xs * m.pi) for xs in x])]
+    noise = lambda x: numpy.random.normal(0, 0.5)
+    pool = Pool(8)
+    multiple_results = []
+    bandits = []
+    for i in range(50):
+        bandits.append(ContextBandit.ContextBandit(lambdas, noise))
+        multiple_results.append(pool.apipe(sim_knn, bandits[i], X, 1, lambda x: 23))
+    data = [res.get() for res in multiple_results]
+    numpy.savetxt('regrets1.csv', numpy.asarray(data), delimiter=',')
